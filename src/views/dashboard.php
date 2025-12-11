645a0f4f30133beb468d53b2d786c5c70bf33f0a
JumpStone <hi@jumpstone4477.de>
2025-12-11 19:49:58 +0100

    Title Change

--- src/views/dashboard.php
+++ src/views/dashboard.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JumpNote Dashboard</title>
+    <title>Neuer Tab</title>
     <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body>
